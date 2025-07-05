a33d18e0671e09023e53dfd7917e40b5d1f1871c
sosuiiii <tanaka_soshi@cyberagent.co.jp>
2025-07-05 22:24:19 +0900

    Initial commit

---
+++ worktree/worktreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  worktreeApp.swift
+//  worktree
+//
+//  Created by 田中 颯志 on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct worktreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ worktreeTests/worktreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  worktreeTests.swift
+//  worktreeTests
+//
+//  Created by 田中 颯志 on 7/5/25.
+//
+
+import Testing
+@testable import worktree
+
+struct worktreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
